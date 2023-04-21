e440e7a5311071fedaef69aff09a246baa2cb064
MelchMwoan <Paarlbergamy@gmail.com>
2023-04-21 19:13:34 +0200

    Created UC-203 tests

---
+++ test/integration/getuserprofile.test.js
@@ -0,0 +1,41 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../app.js');
+chai.should();
+chai.use(chaiHttp);
+
+describe('Register UC-201', function () {
+    it('TC-203-1-InvalidToken', (done) => {
+        //Testing for getting profile with an invalid token
+        //TODO: implement token check
+        chai.request(server).get("/api/profile?token=invalidToken").end((err, res) => {
+            done();
+        })
+    })
+    it('TC-203-2-SuccesfullGettingProfile', (done) => {
+        //Testing for register with invalid email
+        chai.request(server).get("/api/profile?token=validToken").end((err, res) => {
+            res.body.should.be.an("object");
+            res.body.should.have.keys("status", "message", "data");
+            let { data, message, status } = res.body;
+            status.should.equal(200)
+            message.should.be.a("string").that.contains("Profile-endpoint");
+            data.should.be.an("object");
+            data.should.have.keys("id", "firstName", "lastName", "street", "city", "isActive", "emailAddress", "password", "phoneNumber");
+            done();
+        })
+    })
+    it('TC-203-3-GettingProfileWithoutToken', (done) => {
+        //Testing for getting profile without an token
+        chai.request(server).get("/api/profile").end((err, res) => {
+            res.body.should.be.an("object");
+            res.body.should.have.keys("status", "message", "data");
+            let { data, message, status } = res.body;
+            status.should.equal(401)
+            message.should.be.a("string").that.contains("Profile-endpoint: Unauthorized, token is undefined");
+            data.should.be.an("object");
+            data.should.be.empty;
+            done();
+        })
+    })
+})
