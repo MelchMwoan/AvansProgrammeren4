b2376c237729135f03143ef20d6a633daac187b8
MelchMwoan <Paarlbergamy@gmail.com>
2023-04-21 16:08:37 +0200

    Added users

--- routes/user.js
+++ routes/user.js
@@ -21,6 +21,36 @@ const userArray = [{
     emailAddress: "man.vrouw@mail.nl",
     password: "NogRaarder1!",
     phoneNumber: "310612345678"
+}, {
+    id: 3,
+    firstName: "Henk",
+    lastName: "van der Veen",
+    street: "Hogeschoollaan",
+    city: "Breda",
+    isActive: "true",
+    emailAddress: "hvdveen@mail.nl",
+    password: "hvdVeen1!",
+    phoneNumber: "310612345678"
+}, {
+    id: 4,
+    firstName: "Marie",
+    lastName: "van der Veen",
+    street: "Hogeschoollaan",
+    city: "Breda",
+    isActive: "false",
+    emailAddress: "mvdveen@mail.nl",
+    password: "mvdVeen1!",
+    phoneNumber: "310612345678"
+}, {
+    id: 5,
+    firstName: "Leo",
+    lastName: "Hogerhede",
+    street: "Universiteitslaan",
+    city: "Tilburg",
+    isActive: "true",
+    emailAddress: "lhoger@tilburguni.nl",
+    password: "Uni1Til!",
+    phoneNumber: "310612345678"
 }]
 
 router.get('/', (req, res) => {
