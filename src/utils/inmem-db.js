41bf1afa44f8346947887609e4ede911c8bbf5c0
MelchMwoan <Paarlbergamy@gmail.com>
2023-04-25 15:52:02 +0200

    Created util inmem-db

---
+++ src/utils/inmem-db.js
@@ -0,0 +1,52 @@
+const database = [{
+    id: 1,
+    firstName: "Henk",
+    lastName: "Jan",
+    street: "Lovensdijkstraat",
+    city: "Breda",
+    isActive: "false",
+    emailAddress: "henk.jan@mail.nl",
+    password: "rAnDoMww1!",
+    phoneNumber: "310612345678"
+}, {
+    id: 2,
+    firstName: "Man",
+    lastName: "Vrouw",
+    street: "RandomWeg",
+    city: "Amstelveen",
+    isActive: "true",
+    emailAddress: "man.vrouw@mail.nl",
+    password: "NogRaarder1!",
+    phoneNumber: "310612345678"
+}, {
+    id: 3,
+    firstName: "Henk",
+    lastName: "van der Veen",
+    street: "Hogeschoollaan",
+    city: "Breda",
+    isActive: "true",
+    emailAddress: "hvdveen@mail.nl",
+    password: "hvdVeen1!",
+    phoneNumber: "310612345678"
+}, {
+    id: 4,
+    firstName: "Marie",
+    lastName: "van der Veen",
+    street: "Hogeschoollaan",
+    city: "Breda",
+    isActive: "false",
+    emailAddress: "mvdveen@mail.nl",
+    password: "mvdVeen1!",
+    phoneNumber: "310612345678"
+}, {
+    id: 5,
+    firstName: "Leo",
+    lastName: "Hogerhede",
+    street: "Universiteitslaan",
+    city: "Tilburg",
+    isActive: "true",
+    emailAddress: "lhoger@tilburguni.nl",
+    password: "Uni1Til!",
+    phoneNumber: "310612345678"
+}]
+module.exports = database;
