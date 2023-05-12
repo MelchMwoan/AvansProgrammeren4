17e84cbc909dc3ddaed6f84034455b3c65ee5467
MelchMwoan <Paarlbergamy@gmail.com>
2023-05-12 13:18:32 +0200

    Updated tests for typo in emailAddress

--- test/integration/register.test.js
+++ test/integration/register.test.js
@@ -8,7 +8,7 @@ const dbconnection = require('../../src/utils/mysql-db.js');
 describe('Register UC-201', function () {
     it('TC-201-1-InputMissing', (done) => {
         //Testing for register with missing phoneNumber
-        chai.request(server).post("/api/register?firstName=testfirst&lastName=testlast&street=teststreet&city=testcity&emailAdress=t.TC2011@UC201.nl&password=Testpassword1!").end((err, res) => {
+        chai.request(server).post("/api/register?firstName=testfirst&lastName=testlast&street=teststreet&city=testcity&emailAddress=t.TC2011@UC201.nl&password=Testpassword1!").end((err, res) => {
             res.body.should.be.an("object");
             res.body.should.have.keys("status", "message", "data");
             let { data, message, status } = res.body;
@@ -21,12 +21,12 @@ describe('Register UC-201', function () {
     })
     it('TC-201-2-InvalidEmail', (done) => {
         //Testing for register with invalid email
-        chai.request(server).post("/api/register?firstName=testfirst&lastName=testlast&street=teststreet&city=testcity&emailAdress=invalid&password=Testpassword1!&phoneNumber=06-12345678").end((err, res) => {
+        chai.request(server).post("/api/register?firstName=testfirst&lastName=testlast&street=teststreet&city=testcity&emailAddress=invalid&password=Testpassword1!&phoneNumber=06-12345678").end((err, res) => {
             res.body.should.be.an("object");
             res.body.should.have.keys("status", "message", "data");
             let { data, message, status } = res.body;
             status.should.equal(400)
-            message.should.be.a("string").that.contains("Register-endpoint: Bad Request, \"emailAdress\" must be a valid email");
+            message.should.be.a("string").that.contains("Register-endpoint: Bad Request, \"emailAddress\" must be a valid email");
             data.should.be.an("object");
             data.should.be.empty;
             done();
@@ -34,7 +34,7 @@ describe('Register UC-201', function () {
     })
     it('TC-201-3-InvalidPassword', (done) => {
         //Testing for register with invalid password
-        chai.request(server).post("/api/register?firstName=testfirst&lastName=testlast&street=teststreet&city=testcity&emailAdress=t.TC2013@UC201.nl&password=invalid&phoneNumber=06-12345678").end((err, res) => {
+        chai.request(server).post("/api/register?firstName=testfirst&lastName=testlast&street=teststreet&city=testcity&emailAddress=t.TC2013@UC201.nl&password=invalid&phoneNumber=06-12345678").end((err, res) => {
             res.body.should.be.an("object");
             res.body.should.have.keys("status", "message", "data");
             let { data, message, status } = res.body;
@@ -47,7 +47,7 @@ describe('Register UC-201', function () {
     })
     it('TC-201-4-UserAlreadyExists', (done) => {
         //Testing for already existing user (email=henk.jan@mail.nl)
-        chai.request(server).post("/api/register?firstName=testfirst&lastName=testlast&street=teststreet&city=testcity&emailAdress=j.doe@server.com&password=Testpassword1!&phoneNumber=06-12345678").end((err, res) => {
+        chai.request(server).post("/api/register?firstName=testfirst&lastName=testlast&street=teststreet&city=testcity&emailAddress=j.doe@server.com&password=Testpassword1!&phoneNumber=06-12345678").end((err, res) => {
             res.body.should.be.an("object");
             res.body.should.have.keys("status", "message", "data");
             let { data, message, status } = res.body;
@@ -60,14 +60,14 @@ describe('Register UC-201', function () {
     })
     it('TC-201-5-SuccesfullRegistration', (done) => {
         //Testing for succesfull registration
-        chai.request(server).post("/api/register?firstName=TC-201-5&lastName=TC-201-5&street=teststreet&city=testcity&emailAdress=t.TC20615@UC201.nl&password=Testpassword1!&phoneNumber=06-12345678").end((err, res) => {
+        chai.request(server).post("/api/register?firstName=TC-201-5&lastName=TC-201-5&street=teststreet&city=testcity&emailAddress=t.TC20615@UC201.nl&password=Testpassword1!&phoneNumber=06-12345678").end((err, res) => {
             res.body.should.be.an("object");
             res.body.should.have.keys("status", "message", "data");
             let { data, message, status } = res.body;
             status.should.equal(201)
             message.should.be.a("string").that.contains("Register-endpoint: Created, succesfully created a new user");
             data.should.be.an("object");
-            data.should.have.keys("id", "firstName", "lastName", "street", "city", "isActive", "emailAdress", "password", "phoneNumber", "roles");
+            data.should.have.keys("id", "firstName", "lastName", "street", "city", "isActive", "emailAddress", "password", "phoneNumber", "roles");
             let { isActive, id } = data;
             chai.expect([1,0]).to.include(isActive);
             chai.request(server).delete(`/api/user/${id}?token=randomtoken`).end()
