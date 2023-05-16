430696e69c9b31874359d96cb1095d65822a75cb
MelchMwoan <64213755+MelchMwoan@users.noreply.github.com>
2023-05-16 21:17:13 +0200

    Updated get profile tests for authentication

--- test/integration/getuserprofile.test.js
+++ test/integration/getuserprofile.test.js
@@ -3,32 +3,27 @@ const chaiHttp = require('chai-http');
 const server = require('../../app.js');
 chai.should();
 chai.use(chaiHttp);
-const dbconnection = require('../../src/utils/mysql-db.js');
-const getUserJson = {
-    token: "validtoken"
-}
+const jwt = require('jsonwebtoken');
 
 describe('Get User Profile UC-203', function () {
     it('TC-203-1-InvalidToken', (done) => {
         //Testing for getting profile with an invalid token
         //TODO: implement token check
-        let json = getUserJson;
-        json.token = "invalidtoken"
-        chai.request(server).get("/api/user/profile").send(json).end((err, res) => {
+        chai.request(server).get("/api/user/profile").set('Authorization', "Bearer " + jwt.sign({userId: 1}, 'invalidtoken')).end((err, res) => {
             done();
         })
     })
     it('TC-203-2-SuccesfullGettingProfile', (done) => {
         //Testing for succesfully getting profile
-        chai.request(server).get("/api/user/profile").send(getUserJson).end((err, res) => {
+        chai.request(server).get("/api/user/profile").set('Authorization', "Bearer " + jwt.sign({userId: 1}, process.env.jwtSecretKey)).end((err, res) => {
             res.body.should.be.an("object");
             res.body.should.have.keys("status", "message", "data");
             let { data, message, status } = res.body;
             status.should.equal(200)
             message.should.be.a("string").that.contains("Profile-endpoint");
             data.should.be.an("object");
-            // data.should.have.keys("id", "firstName", "lastName", "street", "city", "isActive", "emailAddress", "password", "phoneNumber");
-            data.should.be.empty;
+            data.should.have.keys("id", "firstName", "lastName", "street", "city", "isActive", "emailAddress", "roles", "phoneNumber");
+            // data.should.be.empty;
             done();
         })
     })
@@ -39,7 +34,7 @@ describe('Get User Profile UC-203', function () {
             res.body.should.have.keys("status", "message", "data");
             let { data, message, status } = res.body;
             status.should.equal(401)
-            message.should.be.a("string").that.contains("Profile-endpoint: Unauthorized, \"token\" is required");
+            message.should.be.a("string").that.contains("Authentication-endpoint: Not Authorized, \"authorization\" is required");
             data.should.be.an("object");
             data.should.be.empty;
             done();
