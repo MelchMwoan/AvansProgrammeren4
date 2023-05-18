694bfae06d06ac2217e108a682ca1ac151630c21
MelchMwoan <64213755+MelchMwoan@users.noreply.github.com>
2023-05-18 21:47:29 +0200

    Updated tests for delete user by id

--- test/integration/deleteUserById.test.js
+++ test/integration/deleteUserById.test.js
@@ -23,7 +23,7 @@ describe('Delete User By Id UC-206', function () {
             res.body.should.have.keys("status", "message", "data");
             let { data, message, status } = res.body;
             status.should.equal(404)
-            message.should.be.a("string").that.contains("Userdata Delete-endpoint: Not Found, User with ID #9999 not found");
+            message.should.be.a("string").that.equal("Userdata Delete-endpoint: Not Found, User with ID #9999 not found");
             data.should.be.an("object");
             data.should.be.empty;
             done();
@@ -31,20 +31,32 @@ describe('Delete User By Id UC-206', function () {
     })
     it('TC-206-2-NotLoggedIn', (done) => {
         //Testing for deleting user without being logged in
-        //TODO: test for logged in
-        chai.request(server).delete("/api/user/xxxx").end((err, res) => {
+        chai.request(server).delete("/api/user/1").end((err, res) => {
+            res.body.should.be.an("object");
+            res.body.should.have.keys("status", "message", "data");
+            let { data, message, status } = res.body;
+            status.should.equal(401)
+            message.should.be.a("string").that.equal("Authentication-endpoint: Not Authorized, \"authorization\" is required");
+            data.should.be.an("object");
+            data.should.be.empty;
             done();
         })
     })
     it('TC-206-3-DeleterIsNotOwnerOfData', (done) => {
         //Testing for deleting user details with Id without being owner
-        //TODO: Testing for ownership through token
-        chai.request(server).delete("/api/user/xxxx").end((err, res) => {
+        chai.request(server).delete("/api/user/2").set('Authorization', "Bearer " + jwt.sign({userId: 1}, process.env.jwtSecretKey)).end((err, res) => {
+            res.body.should.be.an("object");
+            res.body.should.have.keys("status", "message", "data");
+            let { data, message, status } = res.body;
+            status.should.equal(403)
+            message.should.be.a("string").that.equal("Userdata Delete-endpoint: Forbidden, you are not the owner of the account with id #2");
+            data.should.be.an("object");
+            data.should.be.empty;
             done();
         })
     })
     it('TC-206-4-UserSuccesfullyDeleted', (done) => {
-        //Testing for deleting user
+        //Testing for deleting user details with Id
         chai.request(server).post("/api/user").send(newUser).end((err, res) => {
             const id = res.body.data.id;
             chai.request(server).delete(`/api/user/${id}`).set('Authorization', "Bearer " + jwt.sign({userId: id}, process.env.jwtSecretKey)).end((err, res) => {
@@ -52,7 +64,7 @@ describe('Delete User By Id UC-206', function () {
                 res.body.should.have.keys("status", "message", "data");
                 let { data, message, status } = res.body;
                 status.should.equal(200)
-                message.should.be.a("string").that.contains(`Gebruiker met ID ${id} is verwijderd`);
+                message.should.be.a("string").that.equal(`Gebruiker met ID ${id} is verwijderd`);
                 data.should.be.an("object");
                 data.should.be.empty;
                 done();
