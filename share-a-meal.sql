edd858deadbfec1f8961f8bc0ae09709d5f1924e
MelchMwoan <64213755+MelchMwoan@users.noreply.github.com>
2023-05-21 15:35:47 +0200

    Updated SQL

--- share-a-meal.sql
+++ share-a-meal.sql
@@ -51,11 +51,11 @@ CREATE TABLE `meal` (
 LOCK TABLES `meal` WRITE;
 /*!40000 ALTER TABLE `meal` DISABLE KEYS */;
 INSERT INTO `meal` VALUES 
-(1,1,0,0,1,'2022-03-22 17:35:00',4,12.75,'https://miljuschka.nl/wp-content/uploads/2021/02/Pasta-bolognese-3-2.jpg',1,'2022-02-26 18:12:40.048998','2022-04-26 12:33:51.000000','Pasta Bolognese met tomaat, spekjes en kaas','Een heerlijke klassieker! Altijd goed voor tevreden gesmikkel!','gluten,lactose'),
-(2,1,1,0,0,'2022-05-22 13:35:00',4,12.75,'https://static.ah.nl/static/recepten/img_RAM_PRD159322_1024x748_JPG.jpg',2,'2022-02-26 18:12:40.048998','2022-04-25 12:56:05.000000','Aubergine uit de oven met feta, muntrijst en tomatensaus','Door aubergines in de oven te roosteren worden ze heerlijk zacht. De balsamico maakt ze heerlijk zoet.','noten'),
-(3,1,0,0,1,'2022-05-22 17:30:00',4,10.75,'https://static.ah.nl/static/recepten/img_099918_1024x748_JPG.jpg',2,'2022-02-26 18:12:40.048998','2022-03-15 14:10:19.000000','Spaghetti met tapenadekip uit de oven en frisse salade','Perfect voor doordeweeks, maar ook voor gasten tijdens een feestelijk avondje.','gluten,lactose'),
-(4,1,0,0,0,'2022-03-26 21:22:26',4,4.00,'https://static.ah.nl/static/recepten/img_063387_890x594_JPG.jpg',3,'2022-03-06 21:23:45.419085','2022-03-12 19:51:57.000000','Zuurkool met spekjes','Heerlijke zuurkoolschotel, dé winterkost bij uitstek. ',''),
-(5,1,1,0,1,'2022-03-26 21:24:46',6,6.75,'https://www.kikkoman.nl/fileadmin/_processed_/5/7/csm_WEB_Bonte_groenteschotel_6851203953.jpg',3,'2022-03-06 21:26:33.048938','2022-03-12 19:50:13.000000','Groentenschotel uit de oven','Misschien wel de lekkerste schotel uit de oven! En vol vitaminen! Dat wordt smikkelen. Als je van groenten houdt ben je van harte welkom. Wel eerst even aanmelden.','');
+(1,1,0,0,1,'2023-03-22 17:35:00',4,12.75,'https://miljuschka.nl/wp-content/uploads/2021/02/Pasta-bolognese-3-2.jpg',1,'2022-02-26 18:12:40.048998','2022-04-26 12:33:51.000000','Pasta Bolognese met tomaat, spekjes en kaas','Een heerlijke klassieker! Altijd goed voor tevreden gesmikkel!','gluten,lactose'),
+(2,1,1,0,0,'2023-05-24 13:35:00',4,12.75,'https://static.ah.nl/static/recepten/img_RAM_PRD159322_1024x748_JPG.jpg',2,'2022-02-26 18:12:40.048998','2022-04-25 12:56:05.000000','Aubergine uit de oven met feta, muntrijst en tomatensaus','Door aubergines in de oven te roosteren worden ze heerlijk zacht. De balsamico maakt ze heerlijk zoet.','noten'),
+(3,1,0,0,1,'2023-05-22 17:30:00',4,10.75,'https://static.ah.nl/static/recepten/img_099918_1024x748_JPG.jpg',2,'2022-02-26 18:12:40.048998','2022-03-15 14:10:19.000000','Spaghetti met tapenadekip uit de oven en frisse salade','Perfect voor doordeweeks, maar ook voor gasten tijdens een feestelijk avondje.','gluten,lactose'),
+(4,1,0,0,0,'2023-06-26 21:22:26',4,4.00,'https://static.ah.nl/static/recepten/img_063387_890x594_JPG.jpg',3,'2022-03-06 21:23:45.419085','2022-03-12 19:51:57.000000','Zuurkool met spekjes','Heerlijke zuurkoolschotel, dé winterkost bij uitstek. ',''),
+(5,1,1,0,1,'2023-04-26 21:24:46',6,6.75,'https://www.kikkoman.nl/fileadmin/_processed_/5/7/csm_WEB_Bonte_groenteschotel_6851203953.jpg',3,'2022-03-06 21:26:33.048938','2022-03-12 19:50:13.000000','Groentenschotel uit de oven','Misschien wel de lekkerste schotel uit de oven! En vol vitaminen! Dat wordt smikkelen. Als je van groenten houdt ben je van harte welkom. Wel eerst even aanmelden.','');
 /*!40000 ALTER TABLE `meal` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -125,11 +125,11 @@ CREATE TABLE `user` (
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 INSERT INTO `user` VALUES 
-(1,'Mariëtte','van den Dullemen',1,'m.vandullemen@server.nl','$2b$10$GI/IPXLIIXxGd80dTgEy3evddGDphAH.erttIpa0c0xbHVdajTTp.','06-12345678','','','Breda'),
-(2,'John','Doe',1,'j.doe@server.com','$2b$10$GI/IPXLIIXxGd80dTgEy3evddGDphAH.erttIpa0c0xbHVdajTTp.','06 12425475','editor,guest','',''),
-(3,'Herman','Huizinga',1,'h.huizinga@server.nl','$2b$10$GI/IPXLIIXxGd80dTgEy3evddGDphAH.erttIpa0c0xbHVdajTTp.','06-12345678','editor,guest','',''),
-(4,'Marieke','Van Dam',0,'m.vandam@server.nl','$2b$10$GI/IPXLIIXxGd80dTgEy3evddGDphAH.erttIpa0c0xbHVdajTTp.','06-12345678','editor,guest','',''),
-(5,'Henk','Tank',1,'h.tank@server.com','$2b$10$GI/IPXLIIXxGd80dTgEy3evddGDphAH.erttIpa0c0xbHVdajTTp.','06 12425495','editor,guest','','');
+(1,'Mariëtte','van den Dullemen',1,'m.vandullemen@server.nl','$2b$10$GI/IPXLIIXxGd80dTgEy3evddGDphAH.erttIpa0c0xbHVdajTTp.','06-12345678','guest','Lovensdijkstraat','Breda'),
+(2,'John','Doe',1,'j.doe@server.com','$2b$10$GI/IPXLIIXxGd80dTgEy3evddGDphAH.erttIpa0c0xbHVdajTTp.','06 12425475','editor,guest','Hoofdstraat','Gorinchem'),
+(3,'Herman','Huizinga',1,'h.huizinga@server.nl','$2b$10$GI/IPXLIIXxGd80dTgEy3evddGDphAH.erttIpa0c0xbHVdajTTp.','06-12345678','editor,guest','Lageweg','Den Bosch'),
+(4,'Marieke','Van Dam',0,'m.vandam@server.nl','$2b$10$GI/IPXLIIXxGd80dTgEy3evddGDphAH.erttIpa0c0xbHVdajTTp.','06-12345678','editor,guest','Stationsplein','Rotterdam'),
+(5,'Henk','Tank',1,'h.tank@server.com','$2b$10$GI/IPXLIIXxGd80dTgEy3evddGDphAH.erttIpa0c0xbHVdajTTp.','06 12425495','editor,guest','Justitiaplein','Utrecht');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
